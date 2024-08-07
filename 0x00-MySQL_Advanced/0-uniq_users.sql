4d01346952cf032ee2da84a393a997db9fdd1233
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-08-07 20:59:23 +0000

    Create table user

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,14 +1,7 @@
--- Ensure compatibility with various SQL modes
-SET sql_mode = '';
+-- a SQL script that creates a table users following requirements
 
--- Check if the 'users' table exists
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users(
+    id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
-
--- Ensure the attributes are unique directly in the table schema to enforce business rules and avoid application bugs
-ALTER TABLE users 
-    ADD CONSTRAINT uq_email UNIQUE (email);
-
