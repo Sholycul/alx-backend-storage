399dbb2d242b9321da388110809dbbb741b51ddc
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-08-09 19:43:31 +0000

    ComputeAverageWeightedScoreForUsers

--- 0x00-MySQL_Advanced/101-main.sql
+++ 0x00-MySQL_Advanced/101-main.sql
@@ -3,7 +3,8 @@ SELECT * FROM users;
 SELECT * FROM projects;
 SELECT * FROM corrections;
 
-CALL ComputeAverageWeightedScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
+CALL ComputeAverageWeightedScoreForUsers();
 
 SELECT "--";
 SELECT * FROM users;
+
